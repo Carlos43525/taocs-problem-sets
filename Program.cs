eca07c58557ec2036444227971deda2f40e9dad1
Carlos43525 <Carlos43525@gmail.com>
2021-12-02 14:28:23 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace taocs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
