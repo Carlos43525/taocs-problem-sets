8f94b00ca67114150d4cee315538526bb7f226a5
Carlos43525 <Carlos43525@gmail.com>
2021-12-02 19:54:00 +0000

    Docker files and devcontainer added.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace taocs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); 
         }
     }
 }
